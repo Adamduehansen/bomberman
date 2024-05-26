1021149525e2baa25023d3450c91eee9a02b5286
Adam Due Hansen <adamduehan@gmail.com>
2024-05-26 22:13:34 +0200

    Implements animation for player directions.

--- src/Player.ts
+++ src/Player.ts
@@ -1,9 +1,12 @@
 import {
   Actor,
   ActorArgs,
+  Animation,
+  AnimationStrategy,
   CollisionType,
   Engine,
   Keys,
+  range,
   Shape,
 } from "excalibur";
 import { spriteSheet } from "./resources.ts";
@@ -20,21 +23,66 @@ export default class Player extends Actor {
   }
 
   onInitialize(): void {
-    this.graphics.use(spriteSheet.getSprite(5, 0));
+    this.graphics.add("idle", spriteSheet.getSprite(4, 0));
+    this.graphics.add(
+      "walk-down",
+      Animation.fromSpriteSheet(
+        spriteSheet,
+        range(3, 5),
+        100,
+        AnimationStrategy.PingPong,
+      ),
+    );
+    this.graphics.add(
+      "walk-up",
+      Animation.fromSpriteSheet(
+        spriteSheet,
+        range(19, 21),
+        100,
+        AnimationStrategy.PingPong,
+      ),
+    );
+    this.graphics.add(
+      "walk-left",
+      Animation.fromSpriteSheet(
+        spriteSheet,
+        range(0, 2),
+        100,
+        AnimationStrategy.PingPong,
+      ),
+    );
+    this.graphics.add(
+      "walk-right",
+      Animation.fromSpriteSheet(
+        spriteSheet,
+        range(16, 18),
+        100,
+        AnimationStrategy.PingPong,
+      ),
+    );
+    this.graphics.use("idle");
   }
 
   onPreUpdate(engine: Engine): void {
     if (engine.input.keyboard.isHeld(Keys.S)) {
+      this.graphics.use("walk-down");
       this.pos.y += 1;
     }
     if (engine.input.keyboard.isHeld(Keys.W)) {
+      this.graphics.use("walk-up");
       this.pos.y -= 1;
     }
     if (engine.input.keyboard.isHeld(Keys.A)) {
+      this.graphics.use("walk-left");
       this.pos.x -= 1;
     }
     if (engine.input.keyboard.isHeld(Keys.D)) {
+      this.graphics.use("walk-right");
       this.pos.x += 1;
+    }
+
+    if (engine.input.keyboard.getKeys().length === 0) {
+      this.graphics.use("idle");
     }
   }
 }
