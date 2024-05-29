edc980ff76e9d4e1232177ec2d559a4221f9b253
Adam Due Hansen <adamduehan@gmail.com>
2024-05-29 07:17:28 +0200

    Separates arm genereting to functions.

--- src/Explosion.ts
+++ src/Explosion.ts
@@ -14,46 +14,13 @@ export default class Explosion extends Actor {
     explosionCenter.graphics.use(spriteSheet.getSprite(2, 6));
     this.addChild(explosionCenter);
 
-    // Right arm
-    const explosionRightArm = new Actor({
-      x: 16,
-    });
-    explosionRightArm.graphics.use(spriteSheet.getSprite(3, 6));
-    this.addChild(explosionRightArm);
+    this.#createTopArm();
+    this.#createRightArm();
+    this.#createBottomArm();
+    this.#createLeftArm();
+  }
 
-    const explosionRightEnd = new Actor({
-      x: 32,
-    });
-    explosionRightEnd.graphics.use(spriteSheet.getSprite(4, 6));
-    this.addChild(explosionRightEnd);
-
-    // Bottom arm
-    const explosionBottomArm = new Actor({
-      y: 16,
-    });
-    explosionBottomArm.graphics.use(spriteSheet.getSprite(2, 7));
-    this.addChild(explosionBottomArm);
-
-    const explosionBottomEnd = new Actor({
-      y: 32,
-    });
-    explosionBottomEnd.graphics.use(spriteSheet.getSprite(2, 8));
-    this.addChild(explosionBottomEnd);
-
-    // Left arm
-    const explosionLeftArm = new Actor({
-      x: -16,
-    });
-    explosionLeftArm.graphics.use(spriteSheet.getSprite(1, 6));
-    this.addChild(explosionLeftArm);
-
-    const explosionLeftEnd = new Actor({
-      x: -32,
-    });
-    explosionLeftEnd.graphics.use(spriteSheet.getSprite(0, 6));
-    this.addChild(explosionLeftEnd);
-
-    // Top arm
+  #createTopArm() {
     const explosionTopArm = new Actor({
       y: -16,
     });
@@ -65,5 +32,47 @@ export default class Explosion extends Actor {
     });
     explosionTopEnd.graphics.use(spriteSheet.getSprite(2, 4));
     this.addChild(explosionTopEnd);
+  }
+
+  #createRightArm() {
+    const explosionRightArm = new Actor({
+      x: 16,
+    });
+    explosionRightArm.graphics.use(spriteSheet.getSprite(3, 6));
+    this.addChild(explosionRightArm);
+
+    const explosionRightEnd = new Actor({
+      x: 32,
+    });
+    explosionRightEnd.graphics.use(spriteSheet.getSprite(4, 6));
+    this.addChild(explosionRightEnd);
+  }
+
+  #createBottomArm() {
+    const explosionBottomArm = new Actor({
+      y: 16,
+    });
+    explosionBottomArm.graphics.use(spriteSheet.getSprite(2, 7));
+    this.addChild(explosionBottomArm);
+
+    const explosionBottomEnd = new Actor({
+      y: 32,
+    });
+    explosionBottomEnd.graphics.use(spriteSheet.getSprite(2, 8));
+    this.addChild(explosionBottomEnd);
+  }
+
+  #createLeftArm() {
+    const explosionLeftArm = new Actor({
+      x: -16,
+    });
+    explosionLeftArm.graphics.use(spriteSheet.getSprite(1, 6));
+    this.addChild(explosionLeftArm);
+
+    const explosionLeftEnd = new Actor({
+      x: -32,
+    });
+    explosionLeftEnd.graphics.use(spriteSheet.getSprite(0, 6));
+    this.addChild(explosionLeftEnd);
   }
 }
