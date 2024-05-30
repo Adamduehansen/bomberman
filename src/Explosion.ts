600640b023524911257c06165e2ea98d7b3c90b9
Adam Due Hansen <adamduehan@gmail.com>
2024-05-30 23:58:15 +0200

    Animates the center explosion.

--- src/Explosion.ts
+++ src/Explosion.ts
@@ -1,6 +1,8 @@
-import { Actor, ActorArgs } from "excalibur";
+import { Actor, ActorArgs, Engine, Timer } from "excalibur";
 import { spriteSheet } from "./resources.ts";
 import map from "./Map.ts";
+
+const NUMBER_OF_FRAMES_IN_ANIMATION = 7;
 
 interface Args extends Required<Pick<ActorArgs, "x" | "y">> {
   maxLength: number;
@@ -8,6 +10,7 @@ interface Args extends Required<Pick<ActorArgs, "x" | "y">> {
 
 export default class Explosion extends Actor {
   #maxLength: number;
+  #graphicsIndex: number;
 
   constructor({ x, y, maxLength }: Args) {
     super({
@@ -15,17 +18,43 @@ export default class Explosion extends Actor {
       y: y,
     });
     this.#maxLength = maxLength;
+    this.#graphicsIndex = 1;
   }
 
-  onInitialize(): void {
+  onInitialize(engine: Engine): void {
     const explosionCenter = new Actor();
-    explosionCenter.graphics.use(spriteSheet.getSprite(2, 6));
+    explosionCenter.graphics.add("1", spriteSheet.getSprite(2, 6));
+    explosionCenter.graphics.add("2", spriteSheet.getSprite(7, 6));
+    explosionCenter.graphics.add("3", spriteSheet.getSprite(2, 11));
+    explosionCenter.graphics.add("4", spriteSheet.getSprite(7, 11));
+    explosionCenter.graphics.add("5", spriteSheet.getSprite(2, 11));
+    explosionCenter.graphics.add("6", spriteSheet.getSprite(7, 6));
+    explosionCenter.graphics.add("7", spriteSheet.getSprite(2, 6));
+    explosionCenter.graphics.use(this.#graphicsIndex.toString());
     this.addChild(explosionCenter);
 
     this.#createTopArm();
     this.#createRightArm();
     this.#createBottomArm();
     this.#createLeftArm();
+
+    const timer = new Timer({
+      fcn: () => {
+        if (this.#graphicsIndex === NUMBER_OF_FRAMES_IN_ANIMATION) {
+          this.kill();
+          return;
+        }
+
+        this.#graphicsIndex += 1;
+        explosionCenter.graphics.use(this.#graphicsIndex.toString());
+      },
+      interval: 200,
+      numberOfRepeats: NUMBER_OF_FRAMES_IN_ANIMATION,
+      repeats: true,
+    });
+
+    engine.addTimer(timer);
+    timer.start();
   }
 
   #createTopArm() {
@@ -114,21 +143,5 @@ export default class Explosion extends Actor {
       }
       this.addChild(explosionLane);
     }
-
-    // if (map.isWallAt(this.pos.x - 16, this.pos.y)) {
-    //   return;
-    // }
-
-    // const explosionLeftArm = new Actor({
-    //   x: -16,
-    // });
-    // explosionLeftArm.graphics.use(spriteSheet.getSprite(1, 6));
-    // this.addChild(explosionLeftArm);
-
-    // const explosionLeftEnd = new Actor({
-    //   x: -32,
-    // });
-    // explosionLeftEnd.graphics.use(spriteSheet.getSprite(0, 6));
-    // this.addChild(explosionLeftEnd);
   }
 }
