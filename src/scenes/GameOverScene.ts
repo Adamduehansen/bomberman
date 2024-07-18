73b34b385513e5f2d3e0627a058dbc74e439f95e
Adam Due Hansen <adamduehan@gmail.com>
2024-07-19 00:07:59 +0200

    feat: a new game can be started from game over screen.

--- src/scenes/GameOverScene.ts
+++ src/scenes/GameOverScene.ts
@@ -1,6 +1,37 @@
-import { Engine, Scene } from "excalibur";
+import { Scene } from "excalibur";
+import { store } from "../store.ts";
+import SceneManager from "../SceneManager.ts";
 
 export default class GameOverScene extends Scene {
-  onInitialize(engine: Engine<any>): void {
+  container?: HTMLDivElement;
+
+  onActivate(): void {
+    console.log("Activating GameOverScene");
+
+    this.container = document.createElement("div");
+    this.container.style.position = "fixed";
+    this.container.style.left = "50%";
+    this.container.style.right = "50%";
+    this.container.style.top = "50%";
+    this.container.style.bottom = "50%";
+    this.container.style.width = "300px";
+
+    const scoreLabel = document.createElement("p");
+    scoreLabel.textContent = `Your score is ${store.getState().score.score}!`;
+
+    this.container.append(scoreLabel);
+
+    const restartButton = document.createElement("button");
+    restartButton.textContent = "Try again";
+    restartButton.onclick = () => {
+      SceneManager.goToScene(this.engine, "gamescene");
+    };
+    this.container.append(restartButton);
+
+    document.body.append(this.container);
+  }
+
+  onDeactivate(): void {
+    this.container!.remove();
   }
 }
