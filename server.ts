475e4b74d7e02a5764aafc22059d9b0fe2cee5e9
Adam Due Hansen <adamduehan@gmail.com>
2025-08-18 20:13:39 +0200

    Server can now serve the dist folder.

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import * as v from "valibot";
-import { Application, Router } from "@oak/oak";
+import { Application, Router, send } from "@oak/oak";
 import {
   InitPlayerForPlayersData,
   ObsoleteConnectionData,
@@ -18,10 +18,6 @@ interface Player {
 const socketMap = new Map<string, WebSocket>();
 
 const router = new Router();
-
-router.get("/", (ctx) => {
-  ctx.response.body = "Hello, World!";
-});
 
 router.get("/ws", (ctx) => {
   if (ctx.isUpgradable === false) {
@@ -147,133 +143,20 @@ router.get("/ws", (ctx) => {
 });
 
 const app = new Application();
+
+app.use(async (ctx, next) => {
+  if (ctx.request.method === "GET" && ctx.request.url.pathname !== "/ws") {
+    await send(ctx, ctx.request.url.pathname, {
+      root: `${Deno.cwd()}/dist`,
+      index: "index.html",
+    });
+    return;
+  }
+  await next();
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen({
   port: 8000,
 });
-
-// Deno.serve((req) => {
-//   const upgradeHeader = req.headers.get("upgrade");
-//   if (upgradeHeader !== "websocket") {
-//     return new Response(null, {
-//       status: 400,
-//       statusText: "Bad Request",
-//     });
-//   }
-//   const { socket: ws, response } = Deno.upgradeWebSocket(req);
-
-//   ws.addEventListener("open", () => {
-//     console.log("Connection established!");
-//     const socketId = crypto.randomUUID();
-//     console.log("Connection has been assigned", socketId);
-//     socketMap.set(socketId, ws);
-//     const otherPlayersData = Object.entries(Object.fromEntries(socketMap)).map((
-//       [id],
-//     ) => ({
-//       id: id,
-//     })).filter(({ id }) => id !== socketId);
-
-//     // Send message to connected player...
-//     const connectionAcceptedData = {
-//       type: "CONNECTION_ACCEPTED",
-//       socketId: socketId,
-//       otherPlayers: otherPlayersData,
-//     };
-//     ws.send(JSON.stringify(connectionAcceptedData));
-//   });
-
-//   ws.addEventListener("message", ({ data }) => {
-//     console.log("Socket message received:", data);
-//     const { success, output, issues } = v.safeParse(
-//       ServerMessageVariantsScheme,
-//       JSON.parse(data),
-//     );
-
-//     if (success === false) {
-//       console.warn("Unhandled type", data);
-//       console.warn(issues);
-//       return;
-//     }
-
-//     switch (output.type) {
-//       case "CONNECTION_CLOSED": {
-//         socketMap.delete(output.socketId);
-
-//         const data: ObsoleteConnectionData = {
-//           type: "OBSOLETE_CONNECTION",
-//           playerId: output.socketId,
-//         };
-//         for (const socket of socketMap.values()) {
-//           socket.send(JSON.stringify(data));
-//         }
-//         break;
-//       }
-//       case "PLAYER_POSITION": {
-//         const { playerId, pos, direction } = output;
-//         for (const [id, socket] of socketMap.entries()) {
-//           if (id === playerId) {
-//             continue;
-//           }
-
-//           const data: PlayerSetPosition = {
-//             type: "PLAYER_SET_POSITION",
-//             playerId: playerId,
-//             pos: pos,
-//             direction: direction,
-//           };
-
-//           socket.send(JSON.stringify(data));
-//         }
-//         break;
-//       }
-//       case "INIT_PLAYER": {
-//         for (const [id, socket] of socketMap.entries()) {
-//           if (id === output.socketId) {
-//             continue;
-//           }
-//           const data: InitPlayerForPlayersData = {
-//             type: "INIT_PLAYERS_FOR_PLAYER",
-//             player: {
-//               playerId: output.socketId,
-//               x: output.pos.x,
-//               y: output.pos.y,
-//             },
-//           };
-//           socket.send(JSON.stringify(data));
-//         }
-//         break;
-//       }
-//       case "SPAWN_BOMB": {
-//         for (const [id, socket] of socketMap.entries()) {
-//           if (id === output.socketId) {
-//             continue;
-//           }
-
-//           socket.send(JSON.stringify(output));
-//         }
-//         break;
-//       }
-//       case "PLAYER_DIE": {
-//         for (const [id, socket] of socketMap.entries()) {
-//           if (id === output.playerId) {
-//             continue;
-//           }
-
-//           socket.send(JSON.stringify(output));
-//         }
-//         break;
-//       }
-//     }
-//   });
-
-//   ws.addEventListener("error", (event) => {
-//     console.log("Socket error:", event);
-//   });
-
-//   ws.addEventListener("close", () => {
-//     console.log("Connection closed!");
-//   });
-
-//   return response;
-// });
